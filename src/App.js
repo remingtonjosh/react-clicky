fe29823715b861cb1338567fab12bcb09befe3e1
Remington Villa <remingtonvilla@gmail.com>
2019-11-15 12:03:14 -0600

    push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>
+          <p>
+            Click on an image to earn points, but don't click on any more than
+            once!
+          </p>
+        </Title>
+
+        {this.state.friends.map(friend => (
+          <FriendCard id={friend.id} key={friend.id} image={friend.image} />
+        ))}
+      </Wrapper>
+    );
+  }
 }
-
 export default App;
